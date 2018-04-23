9792ad0651782c539e98ce9d1412f19cdf534359
zy5163165 <653110669@qq.com>
2018-04-23 09:37:05 +0800

    补充route（snc）两端ctp，并移除vc4层的交叉，补充vc3/vc12层的交叉

--- src/org/asb/mule/probe/ptn/u2000V16/nbi/task/PhysicalDataTask.java
+++ src/org/asb/mule/probe/ptn/u2000V16/nbi/task/PhysicalDataTask.java
@@ -1,16 +1,27 @@
 package org.asb.mule.probe.ptn.u2000V16.nbi.task;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
 
-import com.alcatelsbell.nms.util.ObjectUtil;
-import org.asb.mule.probe.framework.entity.*;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.asb.mule.probe.framework.entity.CTP;
+import org.asb.mule.probe.framework.entity.CrossConnect;
+import org.asb.mule.probe.framework.entity.Equipment;
+import org.asb.mule.probe.framework.entity.EquipmentHolder;
+import org.asb.mule.probe.framework.entity.PTP;
+import org.asb.mule.probe.framework.entity.SubnetworkConnection;
 import org.asb.mule.probe.framework.nbi.task.CommonDataTask;
 import org.asb.mule.probe.framework.service.Constant;
-import org.asb.mule.probe.framework.service.SqliteService;
-
-import com.alcatelsbell.nms.valueobject.BObject;
 import org.asb.mule.probe.ptn.u2000V16.nbi.job.CTPUtil;
 import org.asb.mule.probe.ptn.u2000V16.service.U2000Service;
+
+import com.alcatelsbell.nms.common.Detect;
+import com.alcatelsbell.nms.util.ObjectUtil;
+import com.alcatelsbell.nms.valueobject.BObject;
 
 public class PhysicalDataTask extends CommonDataTask {
 	// private Logger logger = ProbeLog.getInstance().getDataLog();
@@ -24,16 +35,13 @@ public class PhysicalDataTask extends CommonDataTask {
 			try {
 				List<CrossConnect> ipccList = service.retrieveAllCrossConnects(this.getTask().getObjectName());
 				for (CrossConnect ipcc : ipccList) {
-					insertToSqliteDB(ipcc);
 					crossConnects.add(ipcc);
 				}
 				
 				List<SubnetworkConnection> sncList = service.retrieveAllSNCs();
-				nbilog.info("first get sncList.size=" + sncList.size());
 				for (SubnetworkConnection snc : sncList) {
 					sncs.add(snc);
 				}
-				nbilog.info("first get sncs.size=" + sncs.size());
 				
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -122,6 +130,10 @@ public class PhysicalDataTask extends CommonDataTask {
 				}
 			}
 
+			for (CrossConnect crossConnect : crossConnects) {
+				insertToSqliteDB(crossConnect);
+			}
+			
 		} catch (Exception e) {
 			nbilog.error("PhysicalDataTask.excute Exception:", e);
 		} finally {
@@ -133,10 +145,89 @@ public class PhysicalDataTask extends CommonDataTask {
 	public static void processCTP(List<CTP> ctps,List<CrossConnect> ccs,List<SubnetworkConnection> sncs ) {
 		HashMap<String,CTP> ctpMap = new HashMap<String, CTP>();
 		List<String> ccEnds = new ArrayList<>();
+		List<String> sncEnds = new ArrayList<>();
+		HashMap<String,List<String>> vc4DnMap = new HashMap<String, List<String>>();
+		HashMap<String,List<CrossConnect>> vc4ccMap = new HashMap<String, List<CrossConnect>>();
+		List<CTP> newSncCtps = new ArrayList<>();
 		
 		for (CTP ctp : ctps) {
 			ctpMap.put(ctp.getDn(),ctp);
 		}
+		
+		// 补充snc中丢失的ctp (已屏蔽)
+		if (!sncs.isEmpty()) {
+			for (SubnetworkConnection snc : sncs) {
+				String aends = snc.getaEnd();
+				if (aends != null) {
+					String[] dns = aends.split(Constant.listSplitReg);
+					for (String dn : dns) {
+//						ccEnds.add(dn);
+						if (!ctpMap.containsKey(dn)) {
+							sncEnds.add(dn);
+
+//							CTP newCTP = newCTP(dn);
+//							ctps.add(newCTP);
+//							ctpMap.put(dn,newCTP);
+						}
+					}
+				}
+				
+				String zends = snc.getzEnd();
+				if (zends != null) {
+					String[] dns = zends.split(Constant.listSplitReg);
+					for (String dn : dns) {
+//						ccEnds.add(dn);
+						if (!ctpMap.containsKey(dn)) {
+							sncEnds.add(dn);
+
+//							CTP newCTP = newCTP(dn);
+//							ctps.add(newCTP);
+//							ctpMap.put(dn,newCTP);
+						}
+					}
+				}
+			}
+		}
+		// 把snc中丢失的vc12，取上层的vc4 (无需屏蔽)
+		if (Detect.notEmpty(sncEnds)) {
+			for (String sncEnd : sncEnds) {
+				if (CTPUtil.isVC12(sncEnd)) { // vc12
+					String vc4dn = StringUtils.substringBefore(sncEnd, "/vt2_tu12");
+					if (vc4DnMap.containsKey(vc4dn)) {
+						List<String> vc12dns = vc4DnMap.get(vc4dn);
+						vc12dns.add(sncEnd);
+					} else {
+						List<String> vc12dns = new ArrayList<String>();
+						vc4DnMap.put(vc4dn, vc12dns);
+						vc12dns.add(sncEnd);
+					}
+					
+					if (!vc4ccMap.containsKey(vc4dn)) {
+						List<CrossConnect> vc4cc = new ArrayList<CrossConnect>();
+						vc4ccMap.put(vc4dn, vc4cc);
+					}
+				}
+				
+				if (CTPUtil.isVC3(sncEnd)) { // vc3
+					String vc4dn = StringUtils.substringBefore(sncEnd, "/tu3_vc3-k");
+					if (vc4DnMap.containsKey(vc4dn)) {
+						List<String> vc12dns = vc4DnMap.get(vc4dn);
+						vc12dns.add(sncEnd);
+					} else {
+						List<String> vc12dns = new ArrayList<String>();
+						vc4DnMap.put(vc4dn, vc12dns);
+						vc12dns.add(sncEnd);
+					}
+					
+					if (!vc4ccMap.containsKey(vc4dn)) {
+						List<CrossConnect> vc4cc = new ArrayList<CrossConnect>();
+						vc4ccMap.put(vc4dn, vc4cc);
+					}
+				}
+			}
+		}
+		
+		
 		if (!ccs.isEmpty()) {
 			for (CrossConnect cc : ccs) {
 				String aends = cc.getaEndNameList();
@@ -149,6 +240,12 @@ public class PhysicalDataTask extends CommonDataTask {
 
 							ctps.add(newCTP);
 							ctpMap.put(dn,newCTP);
+						}
+						
+						if (vc4ccMap.containsKey(dn)) { // 把snc中丢失的vc12上层的vc4的交叉拿出来
+							cc.setTag3("remove"); //上标记，不做入库处理
+							List<CrossConnect> vc4cc = vc4ccMap.get(dn);
+							vc4cc.add(cc);
 						}
 					}
 				}
@@ -164,41 +261,53 @@ public class PhysicalDataTask extends CommonDataTask {
 							ctps.add(newCTP);
 							ctpMap.put(dn,newCTP);
 						}
-					}
-				}
-			}
-		}
-		if (!sncs.isEmpty()) {
-			for (SubnetworkConnection snc : sncs) {
-				String aends = snc.getaEnd();
-				if (aends != null) {
-					String[] dns = aends.split(Constant.listSplitReg);
-					for (String dn : dns) {
-						ccEnds.add(dn);
-						if (!ctpMap.containsKey(dn)) {
-							CTP newCTP = newCTP(dn);
-
-							ctps.add(newCTP);
-							ctpMap.put(dn,newCTP);
-						}
-					}
-				}
-				
-				String zends = snc.getzEnd();
-				if (zends != null) {
-					String[] dns = zends.split(Constant.listSplitReg);
-					for (String dn : dns) {
-						ccEnds.add(dn);
-						if (!ctpMap.containsKey(dn)) {
-							CTP newCTP = newCTP(dn);
-
-							ctps.add(newCTP);
-							ctpMap.put(dn,newCTP);
+						
+						if (vc4ccMap.containsKey(dn)) { // 把snc中丢失的vc12上层的vc4的交叉拿出来
+							cc.setTag3("remove"); //上标记，不做入库处理
+							List<CrossConnect> vc4cc = vc4ccMap.get(dn);
+							vc4cc.add(cc);
 						}
 					}
 				}
 			}
 		}
+		
+		// 拼装出vc4层交叉的对端vc3/vc12层ctp (已屏蔽)
+//		if (Detect.notEmpty(vc4ccMap)) {
+//			Set<String> vc4dns = vc4ccMap.keySet();
+//			for (String vc4dn : vc4dns) {
+//				List<CrossConnect> vc4ccs = vc4ccMap.get(vc4dn);
+//				if (Detect.notEmpty(vc4ccs)) {
+//					for(CrossConnect vc4cc : vc4ccs) {
+//						vc4cc.setTag3("remove"); //上标记，不做入库处理
+//						String otherEnd = "";
+//						if (vc4cc.getaEndNameList().contains(vc4dn)) {
+//							otherEnd = vc4cc.getzEndNameList();
+//						} else if (vc4cc.getzEndNameList().contains(vc4dn)) {
+//							otherEnd = vc4cc.getaEndNameList();
+//						}
+//						otherEnd = StringUtils.substringBefore(otherEnd, "||");
+//						
+//						List<String> vc12dns = vc4DnMap.get(vc4dn);
+//						for (String vc12dn : vc12dns) {
+//							if (CTPUtil.isVC3(vc12dn)) {
+//								String newdn = otherEnd + "/tu3_vc3-k" + StringUtils.substringAfter(vc12dn, "/tu3_vc3-k");
+//								CTP newCTP = newCTP(newdn);
+//								ctps.add(newCTP);
+//								ctpMap.put(newdn,newCTP);
+//								
+//							}
+//							if (CTPUtil.isVC12(vc12dn)) {
+//								String newdn = otherEnd + "/vt2_tu12" + StringUtils.substringAfter(vc12dn, "/vt2_tu12");
+//								CTP newCTP = newCTP(newdn);
+//								ctps.add(newCTP);
+//								ctpMap.put(newdn,newCTP);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
 
 		HashMap<String,List<CTP>> portCtpMap = new HashMap<String, List<CTP>>();
 		for (CTP ctp : ctps) {
@@ -218,7 +327,74 @@ public class PhysicalDataTask extends CommonDataTask {
 
 			ctps.addAll(ctpList);
 		}
+		
+		
+		// 单独补充snc缺失的ctp，不走交叉的补充逻辑：暂定是用下面的循环来，判断vc3和vc12，然后vc12走l和m的循环，补充两端ctp，建立新交叉。
+		if (Detect.notEmpty(vc4DnMap)) {
+			Set<String> vc4dns = vc4DnMap.keySet();
+			for (String vc4dn : vc4dns) {
+				// 判断vc4下面的是vc3还是vc12
+				List<String> vc12dns = vc4DnMap.get(vc4dn);
+				int[] vc3k = new int[]{};
+				int i = 0;
+				for (String vc12dn : vc12dns) {
+					if (CTPUtil.isVC3(vc12dn)) {
+						vc3k[i] = CTPUtil.getK(vc12dn);
+						i++;
+					}
+				}
 
+				// 获取对端的vc4，用于拼装对端的vc3/vc12
+				List<CrossConnect> vc4ccs = vc4ccMap.get(vc4dn);
+				String otherEnd = "";
+				if (Detect.notEmpty(vc4ccs)) {
+					for(CrossConnect vc4cc : vc4ccs) {
+						if (vc4cc.getaEndNameList().contains(vc4dn)) {
+							otherEnd = vc4cc.getzEndNameList();
+						} else if (vc4cc.getzEndNameList().contains(vc4dn)) {
+							otherEnd = vc4cc.getaEndNameList();
+						}
+						otherEnd = StringUtils.substringBefore(otherEnd, "||");
+					}
+				}
+				
+				for (int k=1;k<4;k++) {
+					// vc3的补充逻辑：补充两端vc3，再补一个交叉
+					if (ArrayUtils.contains(vc3k, k)) {
+						String onedn = vc4dn + "/tu3_vc3-k=" + k;
+						CTP oneCTP = newCTP(onedn);
+						ctps.add(oneCTP);
+						
+						if (Detect.notEmpty(otherEnd)) { // vc层存在交叉才有补充对端ctp和交叉的意义
+							String otherdn = otherEnd + "/tu3_vc3-k=" + k;
+							CTP otherCTP = newCTP(otherdn);
+							ctps.add(otherCTP);
+							
+							CrossConnect newCC = newCC(onedn, otherdn);
+							ccs.add(newCC);
+						}
+					}
+					
+					// vc12的补充逻辑：补充两端vc12，再补3*7个交叉
+					for (int l=1;l<8;l++) {
+						for (int m=1;m<4;m++) {
+							String onedn = vc4dn + "/vt2_tu12="+k+"-l="+l+"-m="+m;
+							CTP oneCTP = newCTP(onedn);
+							ctps.add(oneCTP);
+							
+							if (Detect.notEmpty(otherEnd)) { // vc层存在交叉才有补充对端ctp和交叉的意义
+								String otherdn = otherEnd + "/vt2_tu12="+k+"-l="+l+"-m="+m;
+								CTP otherCTP = newCTP(otherdn);
+								ctps.add(otherCTP);
+								
+								CrossConnect newCC = newCC(onedn, otherdn);
+								ccs.add(newCC);
+							}
+						}
+					}
+				}
+			}
+		}
 
 	}
 	public static void putIntoValueList(HashMap  map,Object key,Object value) {
@@ -247,6 +423,28 @@ public class PhysicalDataTask extends CommonDataTask {
 			newCTP.setNativeEMSName("VC3-"+CTPUtil.getK(dn));
 
 		return newCTP;
+	}
+	public static  CrossConnect newCC(String oneDn, String otherDn) {
+		CrossConnect newCC = new CrossConnect();
+		String dn = oneDn + "<>" + StringUtils.substringAfter(otherDn, "@PTP:");
+		dn = StringUtils.replace(dn, "@CTP:", "");
+		dn = StringUtils.replace(dn, "PTP", "CrossConnect");
+				
+		newCC.setDn(dn);
+		newCC.setTag1("NEW-SNC");
+		
+		newCC.setaEndNameList(oneDn);
+		newCC.setzEndNameList(otherDn);
+		newCC.setaEndTP(extractPortDn(oneDn));
+		newCC.setzEndTP(extractPortDn(otherDn));
+        newCC.setCcType("ST_SIMPLE");
+        newCC.setDirection("CD_UNI");
+        newCC.setEmsName(StringUtils.substringBefore(oneDn, "@ManagedElement:"));
+        newCC.setParentDn(StringUtils.substringBefore(oneDn, "@PTP:"));
+        newCC.setAdditionalInfo("Label:||");
+        
+        
+		return newCC;
 	}
 	public static String extractPortDn(String endDn) {
 		if (endDn.contains("@CTP"))
@@ -278,6 +476,24 @@ public class PhysicalDataTask extends CommonDataTask {
 
 	public static void main(String[] args) {
 		PhysicalDataTask task1 = new PhysicalDataTask();
+		
+		String oneDn = "EMS:Huawei/U2000@ManagedElement:3145731@PTP:/rack=1/shelf=1/slot=6/domain=sdh/port=1@CTP:/sts3c_au4-j=19/vt2_tu12-k=1-l=5-m=1";
+		String otherDn = "EMS:Huawei/U2000@ManagedElement:3145731@PTP:/rack=1/shelf=1/slot=1/domain=sdh/port=2@CTP:/sts3c_au4-j=1/vt2_tu12-k=1-l=1-m=3";
+		String dn = oneDn + "<>" + StringUtils.substringAfter(otherDn, "@PTP:");
+		dn = StringUtils.replace(dn, "@CTP:", "");
+		dn = StringUtils.replace(dn, "PTP", "CrossConnect");
+		
+		List<String> vc12dns = new ArrayList<>();
+		vc12dns.add("EMS:Huawei/U2000@ManagedElement:3145776@PTP:/rack=1/shelf=1/slot=27/domain=sdh/port=1@CTP:/sts3c_au4-j=5/vt2_tu12-k=1-l=7-m=1");
+		int[] vc3k = new int[]{};
+		int i = 0;
+		for (String vc12dn : vc12dns) {
+			if (CTPUtil.isVC3(vc12dn)) {
+				vc3k[i] = CTPUtil.getK(vc12dn);
+				i++;
+			}
+		}
+		
 		List<CrossConnect> ccs = (List<CrossConnect>) ObjectUtil.readObjectByPath("d:\\cache\\result_1431952368947");
 		List<SubnetworkConnection> sncs = new ArrayList<SubnetworkConnection>();
 		List<CTP> ctps = (List<CTP>)ObjectUtil.readObjectByPath("d:\\cache\\result_1431952312635");
